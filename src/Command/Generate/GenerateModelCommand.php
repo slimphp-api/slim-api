6850548041b7d385957c218645f21c0de7e15b28
Gabriel Baker <me@g403.co>
2015-09-25 23:09:07 +0100

    Split generate commands into individual commands

---
+++ src/Command/Generate/GenerateModelCommand.php
@@ -0,0 +1,68 @@
+<?php
+namespace SlimApi\Command\Generate;
+
+use \Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use SlimApi\Generator\GeneratorInterface;
+use SlimApi\Command\Traits\GeneratorTrait;
+
+class GenerateModelCommand extends Command
+{
+    use GeneratorTrait;
+
+    /**
+     * @param GeneratorInterface $generator
+     *
+     * @return
+     */
+    public function __construct(GeneratorInterface $generator)
+    {
+        parent::__construct();
+        $this->generator = $generator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('generate:model')
+            ->setDescription('Generates a model and migration.')
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'Resultant resource name.'
+            )
+            ->addArgument(
+                'fields',
+                InputArgument::IS_ARRAY,
+                'What fields (if appropriate).'
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name   = ucfirst($input->getArgument('name'));
+        $fields = $input->getArgument('fields');
+
+        try {
+            $this->validate($name, $fields);
+            $this->generator->process($name, $fields);
+            $output->writeln('<info>Generation completed.</info>');
+        } catch (Exception $e) {
+            $output->writeln('<info>'.$e->getMessage().'</info>');
+            return 1;
+        }
+
+        return 0;
+    }
+}
